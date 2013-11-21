b3037af8f7e81daee2b0427abc3a1ebbe7120571
Ben Wu <wubin28@gmail.com>
2013-11-21 20:26:54 +0800

    Wrote the test for ordering one coffee.

---
+++ KataStarbuzzCoffee/KataStarbuzzCoffee/KataStarbuzzCoffeeTest.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace KataStarbuzzCoffee
+{
+    [TestFixture]
+    public class KataStarbuzzCoffeeTest
+    {
+        [Test]
+        public void TestOneCoffee()
+        {
+            // Given
+            StarbuzzCoffeeFacade starbuzzCoffeeFacade = new StarbuzzCoffeeFacade();
+            // When
+            StarbuzzCoffeeFacade.OrderOneCoffee();
+            // Then
+            Assert.AreEqual("Coffee (22.00) | Total=22.00", starbuzzCoffeeFacade.printBill(), "Failure - the bill should be printed as 'Coffee (22.00) | Total=22.00'.");
+        }
+    }
+}
