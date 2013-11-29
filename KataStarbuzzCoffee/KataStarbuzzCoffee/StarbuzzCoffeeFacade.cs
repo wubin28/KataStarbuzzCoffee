eea0b17b1d2aaf91d3a1091d76f1db83e98fbfdd
Ben Wu <wubin28@gmail.com>
2013-11-29 13:01:32 +0800

    Created class Honey.

--- KataStarbuzzCoffee/KataStarbuzzCoffee/StarbuzzCoffeeFacade.cs
+++ KataStarbuzzCoffee/KataStarbuzzCoffee/StarbuzzCoffeeFacade.cs
@@ -51,4 +51,22 @@ namespace KataStarbuzzCoffee
         }
 
     }
+
+    internal class Honey : Beverage
+    {
+        public Honey(Beverage beverage)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override string GetTotalDescription()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override int GetTotalPrice()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
 }
