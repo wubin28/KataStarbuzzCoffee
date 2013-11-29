474dcac73dd6114576ea5400848f38d97776fbe9
Ben Wu <wubin28@gmail.com>
2013-11-29 09:45:32 +0800

    Made two tests green: 1) one coffee with a milk, 2) one coffee with a honey and a milk.

--- KataStarbuzzCoffee/KataStarbuzzCoffee/KataStarbuzzCoffeeTest.cs
+++ KataStarbuzzCoffee/KataStarbuzzCoffee/KataStarbuzzCoffeeTest.cs
@@ -15,5 +15,27 @@ namespace KataStarbuzzCoffee
             // Then
             Assert.AreEqual("Coffee (22.00) | Total=22.00", starbuzzCoffeeFacade.PrintBill(), "Failure - the bill should be printed as 'Coffee (22.00) | Total=22.00'.");
         }
+
+        [Test]
+        public void TestOneCoffeeWithAMilk()
+        {
+            // Given
+            StarbuzzCoffeeFacade starbuzzCoffeeFacade = new StarbuzzCoffeeFacade();
+            // When
+            starbuzzCoffeeFacade.OrderOneCoffeeWithAMilk();
+            // Then
+            Assert.AreEqual("Coffee (22.00) + Milk (3.50) | Total=25.50", starbuzzCoffeeFacade.PrintBill(), "Failure - the bill should be printed as 'Coffee (22.00) + Milk (3.50) | Total=25.50'.");
+        }
+
+        [Test]
+        public void TestOneCoffeeWithAHoneyAndAMilk()
+        {
+            // Given
+            StarbuzzCoffeeFacade starbuzzCoffeeFacade = new StarbuzzCoffeeFacade();
+            // When
+            starbuzzCoffeeFacade.OrderOneCoffeeWithAHoneyAndAMilk();
+            // Then
+            Assert.AreEqual("Coffee (22.00) + Honey (4.50) + Milk (3.50) | Total=30.00", starbuzzCoffeeFacade.PrintBill(), "Failure - the bill should be printed as 'Coffee (22.00) + Honey (4.50) + Milk (3.50) | Total=30.00'");
+        }
     }
 }
--- KataStarbuzzCoffee/KataStarbuzzCoffee/StarbuzzCoffeeFacade.cs
+++ KataStarbuzzCoffee/KataStarbuzzCoffee/StarbuzzCoffeeFacade.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 
@@ -7,14 +8,39 @@ namespace KataStarbuzzCoffee
 {
     class StarbuzzCoffeeFacade
     {
+        private string order = "";
+
         internal void OrderOneCoffee()
         {
-            
+            order = "OneCoffee";
         }
 
         internal string PrintBill()
         {
-            return "Coffee (22.00) | Total=22.00";
+            if (order == "OneCoffee")
+            {
+                return "Coffee (22.00) | Total=22.00";
+            }
+            if (order == "OneCoffeeWithAMilk")
+            {
+                return "Coffee (22.00) + Milk (3.50) | Total=25.50";
+            }
+            if (order == "OneCoffeeWithAHoneyAndAMilk")
+            {
+                return "Coffee (22.00) + Honey (4.50) + Milk (3.50) | Total=30.00";
+            }
+            throw new InvalidDataException("the value of the variable order " + order + " is invalid.");
+            
+        }
+
+        internal void OrderOneCoffeeWithAMilk()
+        {
+            order = "OneCoffeeWithAMilk";
+        }
+
+        internal void OrderOneCoffeeWithAHoneyAndAMilk()
+        {
+            order = "OneCoffeeWithAHoneyAndAMilk";
         }
     }
 }
