e0699ac6d2e691a8b2465e4f7c7807ec4e322cf6
Ben Wu <wubin28@gmail.com>
2013-11-29 13:02:37 +0800

    Moved class Honey to another file to match type name.

---
+++ KataStarbuzzCoffee/KataStarbuzzCoffee/Honey.cs
@@ -0,0 +1,20 @@
+﻿namespace KataStarbuzzCoffee
+{
+    internal class Honey : Beverage
+    {
+        public Honey(Beverage beverage)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override string GetTotalDescription()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override int GetTotalPrice()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
--- KataStarbuzzCoffee/KataStarbuzzCoffee/StarbuzzCoffeeFacade.cs
+++ KataStarbuzzCoffee/KataStarbuzzCoffee/StarbuzzCoffeeFacade.cs
@@ -51,22 +51,4 @@ namespace KataStarbuzzCoffee
         }
 
     }
-
-    internal class Honey : Beverage
-    {
-        public Honey(Beverage beverage)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public override string GetTotalDescription()
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public override int GetTotalPrice()
-        {
-            throw new System.NotImplementedException();
-        }
-    }
 }
