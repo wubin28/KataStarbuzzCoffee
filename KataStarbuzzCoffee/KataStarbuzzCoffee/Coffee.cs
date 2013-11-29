5b7b2c5e57f629397bb241b502df50ca0fc3ff38
Ben Wu <wubin28@gmail.com>
2013-11-29 11:03:09 +0800

    Alt-Entered two blank methods in class Coffee.

--- KataStarbuzzCoffee/KataStarbuzzCoffee/Coffee.cs
+++ KataStarbuzzCoffee/KataStarbuzzCoffee/Coffee.cs
@@ -2,5 +2,14 @@
 {
     internal class Coffee : Beverage
     {
+        public override string GetTotalDescription()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override string GetTotalPrice()
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
